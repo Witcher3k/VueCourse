3a702da6ac141c3e61f426daae924c6e18042208
Kamil <witcherek1112@gmail.com>
2019-04-05 18:49:30 +0200

    vue cli

---
+++ vue-cli-lesson/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>status:{{status}}</h1>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return { status: "Critical" };
+  },
+  methods: {
+    changeStatus() {
+      if (this.status === "Critical") {
+        this.status = "Normal";
+      } else {
+        this.status = "Critical";
+      }
+    }
+  }
+};
+</script>
