c6c4cfe1aa76885315112a6798aaacce625a6431
Kamil <witcherek1112@gmail.com>
2019-04-06 12:14:10 +0200

    Ex1 Components

---
+++ ComponentsEx1/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <app-list-group></app-list-group>
+    <hr>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import ListGroup from "./ListGroup.vue";
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+export default {
+  components: {
+    "app-list-group": ListGroup,
+    "app-header": Header,
+    "app-footer": Footer
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ ComponentsEx1/src/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12">
+      <header>
+        <h1>Hello my friend</h1>
+      </header>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
