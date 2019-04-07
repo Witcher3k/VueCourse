ee5dcecccd365e4c9612dd84e6ae84cd71744b5e
Kamil <witcherek1112@gmail.com>
2019-04-07 12:33:07 +0200

    next course part

---
+++ ComponentsEx2/src/components/User.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User! {{name}}</p>
+    <hr>
+    <button @click="changeName">Change my name</button>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail :name="name" @resetName="name=$event"></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  data: function() {
+    return { name: "Kamil" };
+  },
+  methods: {
+    changeName: function() {
+      this.name = "Anna";
+      console.log("zmieniam");
+    }
+  },
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightblue;
+}
+</style>
---
+++ ComponentsEx2/src/components/UserDetail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name:{{name}} {{changeName()}}</p>
+    <button @click="resetName">Reset Name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String
+  },
+  methods: {
+    changeName() {
+      return this.name
+        .split("")
+        .reverse()
+        .join("");
+    },
+    resetName() {
+      this.name = "-";
+      this.$emit("nameReset", this.name);
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>
